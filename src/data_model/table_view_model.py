7ad66f03be3f4dbebaf46e8eccf1cc9fa5601f33
laptop-windows <duong80559@gmail.com>
2026-06-16 23:10:34 +0700

    cập nhật model: nhận data dưới dạng list-tuple hoặc list-dict. tự tách tên cột khỏi chuỗi trong trường hợp join bảng

--- src/data_model/table_view_model.py
+++ src/data_model/table_view_model.py
@@ -21,15 +21,29 @@ class TableViewModel(QAbstractTableModel):
         Hàm này tự động lặp qua các ô trong data, chỉ cần hướng dẫn nó lấy dữ liệu từ ô đầu tiên
         """
         if role == Qt.ItemDataRole.DisplayRole:
-            row_object = self._data[index.row()]
-            col_attr = self._column_names[index.column()]
+            row_data = self._data[index.row()]
 
-            value = getattr(row_object, col_attr, "")
+            # Nếu data có dạng một list hoặc tup thì chỉ cần truy cập từng phần tử trong mỗi row
+            if isinstance(row_data, (tuple, list)):
+                value = row_data[index.column()]
+                return str(value) if value is not None else ""
+
+            # Nếu data là các object thì 
+            col_attr = self._column_names[index.column()]
+            part = col_attr.split(".")
+            value = row_data
+            for part in part:
+                value = getattr(value, part, None)
+                if value is None:
+                    break
+            return str(value) if value is not None else ""
+
+            value = getattr(row_data, col_attr, "")
             return str(value)
 
     def headerData(self, section, orientation, /, role = ...):
         if role == Qt.ItemDataRole.DisplayRole:
             if orientation == Qt.Orientation.Horizontal:
                 return self._column_names[section]
-            return section + 1
+            return str(section + 1)
         return None
